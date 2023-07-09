96eaaad21ee1ca2b3993004526384c6bcd1d2fb9
Suzulu <zukkiyb.iu2@gmail.com>
2023-07-09 11:34:31 +0900

    LocalTimelineのQueryいじった

--- packages/backend/src/server/api/endpoints/notes/local-timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/local-timeline.ts
@@ -75,7 +75,8 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 			const query = this.queryService.makePaginationQuery(this.notesRepository.createQueryBuilder('note'),
 				ps.sinceId, ps.untilId, ps.sinceDate, ps.untilDate)
 				.andWhere('note.id > :minId', { minId: this.idService.genId(new Date(Date.now() - (1000 * 60 * 60 * 24 * 10))) }) // 10日前まで
-				.andWhere('(note.visibility = \'public\') AND (note.userHost IS NULL)')
+				.andWhere('(note.visibility = \'public\') AND (note.renoteId IS NULL) AND (note.channelId IS NULL)')
+				.andWhere('note.renoteId > :minrenoteId',{minrenoteId: 3})
 				.innerJoinAndSelect('note.user', 'user')
 				.leftJoinAndSelect('note.reply', 'reply')
 				.leftJoinAndSelect('note.renote', 'renote')
