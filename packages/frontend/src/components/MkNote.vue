876d26e0ad2b251a0f5911445017682b15288dfe
Suzulu <zukkiyb.iu2@gmail.com>
2023-07-10 21:27:34 +0900

    スポットライトタイムラインのRenote表示を頑張って消してみる

--- packages/frontend/src/components/MkNote.vue
+++ packages/frontend/src/components/MkNote.vue
@@ -14,7 +14,7 @@
 	<div v-if="isRenote" :class="$style.renote">
 		<div v-if="note.channel" :class="$style.colorBar" :style="{ background: note.channel.color }"></div>
 		<MkAvatar :class="$style.renoteAvatar" :user="note.user" link preview/>
-		<i class="ti ti-repeat" style="margin-right: 4px;"></i>
+		<i v-if="	props.src != 'local'" class="ti ti-repeat" style="margin-right: 4px;"></i>
 		<I18n :src="i18n.ts.renotedBy" tag="span" :class="$style.renoteText">
 			<template #user>
 				<MkA v-user-preview="note.userId" :class="$style.renoteUserName" :to="userPage(note.user)">
@@ -193,7 +193,6 @@ const isRenote = (
 	note.text == null &&
 	note.fileIds.length === 0 &&
 	note.poll == null &&
-	props.src != "local"
 );
 
 const el = shallowRef<HTMLElement>();
