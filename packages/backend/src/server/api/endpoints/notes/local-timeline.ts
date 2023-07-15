2158ac4620089592e79e2321a585ef27dfabc85b
Suzulu <zukkiyb.iu2@gmail.com>
2023-07-16 05:32:50 +0900

    distinct実装できるか実験

--- packages/backend/src/server/api/endpoints/notes/local-timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/local-timeline.ts
@@ -96,9 +96,9 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 				.where('following.followerId = :followerId', { followerId: me.id });
 
 			//#region Construct query
-			const query = this.queryService.makePaginationQuery(this.notesRepository.createQueryBuilder('note'),
+			const query = this.queryService.makePaginationQuery(this.notesRepository.createQueryBuilder('note').distinctOn(['renoteId']),
 				ps.sinceId, ps.untilId, ps.sinceDate, ps.untilDate)
-				.distinctOn(['renoteId'])
+				
 				.andWhere('note.id > :minId', { minId: this.idService.genId(new Date(Date.now() - (1000 * 60 * 60 * 24 * 10))) }) // 10日前まで
 				.andWhere(new Brackets(qb => {
 					qb.where(`((note.userId IN (${ followingQuery.getQuery() })) AND (note.renoteCount > :minrenoteCount1) AND (note.renote IS NULL))`,{minrenoteCount1: 5})
