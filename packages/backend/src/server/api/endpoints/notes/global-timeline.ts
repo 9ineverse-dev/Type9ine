9e7bde23252802c4625c31feb8b05b25d17e148b
Suzulu <zukkiyb.iu2@gmail.com>
2023-07-26 23:47:50 +0900

    TypeORMガチでわからないんだけど

--- packages/backend/src/server/api/endpoints/notes/global-timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/global-timeline.ts
@@ -90,7 +90,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 			.select('note.userId')
 			.select('note.userHost')
 			.leftJoinAndSelect('note.renote', 'renote')
-			.where('note.userId IN (:...meOrFolloweeIds)', { meOrFolloweeIds: meOrFolloweeIds })
+			.andwhere('note.userId IN (:...meOrFolloweeIds)', { meOrFolloweeIds: meOrFolloweeIds })
 			.andWhere(new Brackets(qb =>{
 				qb.orWhere('(renote.renoteCount > :drrenoteCounter1)',{drrenoteCounter1:3})
 				.orWhere('(renote.renoteCount > :drrenoteCounter2) AND (renote.userHost IS NULL)',{drrenoteCounter2:2})
@@ -115,12 +115,12 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 			.leftJoinAndSelect('reply.user', 'replyUser')
 			.leftJoinAndSelect('renote.user', 'renoteUser');
 
-		if (followees.length > 0) {
+		if (followees.length > 0 || rnDistinct.length > 0) {
 
 			query.andWhere(new Brackets(qb =>{
 			qb.orWhere('(note.userId IN (:...meOrFolloweeIds)) AND (note.renoteCount > :renoteCounter1)', { meOrFolloweeIds: meOrFolloweeIds ,renoteCounter1:10 });
 			qb.orWhere('(note.renoteCount > :renoteCounter2) AND (note.renote IS NULL)',{renoteCounter2:60});
-			qb.orWhere('note.id IN (:distinctRns)',{ distinctRns : distinctRns});
+			qb.orWhere('note.id IN (:...distinctRns)',{ distinctRns : distinctRns});
 		  }));
 		} else {
 			query.andWhere('note.userId = :meId', { meId: me.id });
