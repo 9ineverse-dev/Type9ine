dd13d3bd70cb3be37b3cd513712c84dd6faa8304
Suzulu <zukkiyb.iu2@gmail.com>
2023-07-28 18:57:23 +0900

    ヒラケ...！シナモンヨ！

--- packages/backend/src/server/api/stream/channels/global-timeline.ts
+++ packages/backend/src/server/api/stream/channels/global-timeline.ts
@@ -40,49 +40,75 @@ class GlobalTimelineChannel extends Channel {
 
 	@bindThis
 	private async onNote(note: Packed<'Note'>) {
-		if (note.visibility !== 'public') return;
-		if (note.channelId != null) return;
+    if(note.id != null) return;//スポットライトの自動更新は一時的に削除
 
-		// リプライなら再pack
-		if (note.replyId != null) {
-			note.reply = await this.noteEntityService.pack(note.replyId, this.user, {
+
+		let DynamicRenoteCount1 = 3;
+		let DynamicRenoteCount2 = 10;
+		let DynamicRenoteCount3 = 15;
+		let DynamicRenoteCount4 = 30;
+		let DynamicRenoteCount5 = 50;
+
+		if (!(
+			(note.channelId == null && note.renote!.renoteCount == DynamicRenoteCount2 && note.user.host == null && note.renoteId != null)||
+			(note.channelId == null && this.following.has(note.renote!.userId) && note.renote!.renoteCount == DynamicRenoteCount1 && note.renoteId != null)||
+			(note.channelId == null && note.renote!.renoteCount == DynamicRenoteCount2 && note.user.host == null && note.renoteId != null))
+		) return;
+
+		let gnote = await this.noteEntityService.pack(note.renoteId, this.user!, {
+			detail: true,
+		});
+
+		if (['followers', 'specified'].includes(note.visibility)) {
+			gnote = await this.noteEntityService.pack(note.renoteId, this.user!, {
 				detail: true,
 			});
+
+			if (note.isHidden) {
+				return;
+			}
+		} else {
+			// リプライなら再pack
+			if (gnote.replyId != null) {
+				gnote.reply = await this.noteEntityService.pack(gnote.replyId, this.user!, {
+					detail: true,
+				});
+			}
+			// Renoteなら再pack
+			if (gnote.renoteId != null) {
+				gnote.renote = await this.noteEntityService.pack(gnote.renoteId, this.user!, {
+					detail: true,
+				});
+			}
 		}
-		// Renoteなら再pack
-		if (note.renoteId != null) {
-			note.renote = await this.noteEntityService.pack(note.renoteId, this.user, {
-				detail: true,
-			});
-		}
+
+		// Ignore notes from instances the user has muted
+		if (isInstanceMuted(gnote, new Set<string>(this.userProfile!.mutedInstances ?? []))) return;
 
 		// 関係ない返信は除外
-		if (note.reply && !this.withReplies) {
-			const reply = note.reply;
+		if (gnote.reply && !this.withReplies) {
+			const reply = gnote.reply;
 			// 「チャンネル接続主への返信」でもなければ、「チャンネル接続主が行った返信」でもなければ、「投稿者の投稿者自身への返信」でもない場合
 			if (reply.userId !== this.user!.id && note.userId !== this.user!.id && reply.userId !== note.userId) return;
 		}
 
-		// Ignore notes from instances the user has muted
-		if (isInstanceMuted(note, new Set<string>(this.userProfile?.mutedInstances ?? []))) return;
-
 		// 流れてきたNoteがミュートしているユーザーが関わるものだったら無視する
-		if (isUserRelated(note, this.userIdsWhoMeMuting)) return;
+		if (isUserRelated(gnote, this.userIdsWhoMeMuting)) return;
 		// 流れてきたNoteがブロックされているユーザーが関わるものだったら無視する
-		if (isUserRelated(note, this.userIdsWhoBlockingMe)) return;
+		if (isUserRelated(gnote, this.userIdsWhoBlockingMe)) return;
 
-		if (note.renote && !note.text && isUserRelated(note, this.userIdsWhoMeMutingRenotes)) return;
+		if (gnote.renote && !gnote.text && isUserRelated(gnote, this.userIdsWhoMeMutingRenotes)) return;
 
 		// 流れてきたNoteがミュートすべきNoteだったら無視する
 		// TODO: 将来的には、単にMutedNoteテーブルにレコードがあるかどうかで判定したい(以下の理由により難しそうではある)
 		// 現状では、ワードミュートにおけるMutedNoteレコードの追加処理はストリーミングに流す処理と並列で行われるため、
 		// レコードが追加されるNoteでも追加されるより先にここのストリーミングの処理に到達することが起こる。
 		// そのためレコードが存在するかのチェックでは不十分なので、改めてcheckWordMuteを呼んでいる
-		if (this.userProfile && await checkWordMute(note, this.user, this.userProfile.mutedWords)) return;
+		if (this.userProfile && await checkWordMute(gnote, this.user, this.userProfile.mutedWords)) return;
 
-		this.connection.cacheNote(note);
+		this.connection.cacheNote(gnote);
 
-		this.send('note', note);
+		this.send('note', gnote);
 	}
 
 	@bindThis
--- packages/backend/src/server/api/stream/channels/local-timeline.ts
+++ packages/backend/src/server/api/stream/channels/local-timeline.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { checkWordMute } from '@/misc/check-word-mute.js';
 import { isUserRelated } from '@/misc/is-user-related.js';
-import { isInstanceMuted } from '@/misc/is-instance-muted.js';
 import type { Packed } from '@/misc/json-schema.js';
 import { MetaService } from '@/core/MetaService.js';
 import { NoteEntityService } from '@/core/entities/NoteEntityService.js';
@@ -12,7 +11,7 @@ import Channel from '../channel.js';
 class LocalTimelineChannel extends Channel {
 	public readonly chName = 'localTimeline';
 	public static shouldShare = true;
-	public static requireCredential = true;
+	public static requireCredential = false;
 	private withReplies: boolean;
 
 	constructor(
@@ -28,7 +27,7 @@ class LocalTimelineChannel extends Channel {
 	}
 
 	@bindThis
-	public async init(params: any): Promise<void> {
+	public async init(params: any) {
 		const policies = await this.roleService.getUserPolicies(this.user ? this.user.id : null);
 		if (!policies.ltlAvailable) return;
 
@@ -37,84 +36,54 @@ class LocalTimelineChannel extends Channel {
 		// Subscribe events
 		this.subscriber.on('notesStream', this.onNote);
 	}
-//将来的にはstreamもソートに準拠させたい
+
 	@bindThis
 	private async onNote(note: Packed<'Note'>) {
-		let DynamicRenoteCount1 = 10;
-		let DynamicRenoteCount2 = 40;
-		let DynamicRenoteCount3 = 60;
-		if(this.connection.user!.followersCount < 50){
-			DynamicRenoteCount1 = 5;
-			DynamicRenoteCount2 = 10;
-			DynamicRenoteCount3 = 20;
-		}else if(this.connection.user!.followersCount < 500){
-			DynamicRenoteCount2 = 20;
-			DynamicRenoteCount3 = 30;
-		}
-		if (!(
-			(note.channelId == null && this.following.has(note.renote!.userId) && note.renote!.renoteCount == DynamicRenoteCount1 && note.renoteId != null)||
-			(note.channelId == null && note.renote!.renoteCount == DynamicRenoteCount2 && note.user.host == null && note.renoteId != null)||
-			(note.channelId == null && note.renote!.renoteCount == DynamicRenoteCount3 && note.renoteId != null))
-		) return;
+		if (note.user.host !== null) return;
+		if (note.visibility !== 'public') return;
+		if (note.channelId != null && !this.followingChannels.has(note.channelId)) return;
 
-		let gnote = await this.noteEntityService.pack(note.renoteId, this.user!, {
-			detail: true,
-		});
-
-		if (['followers', 'specified'].includes(note.visibility)) {
-			gnote = await this.noteEntityService.pack(note.renoteId, this.user!, {
+		// リプライなら再pack
+		if (note.replyId != null) {
+			note.reply = await this.noteEntityService.pack(note.replyId, this.user, {
 				detail: true,
 			});
-
-			if (note.isHidden) {
-				return;
-			}
-		} else {
-			// リプライなら再pack
-			if (gnote.replyId != null) {
-				gnote.reply = await this.noteEntityService.pack(gnote.replyId, this.user!, {
-					detail: true,
-				});
-			}
-			// Renoteなら再pack
-			if (gnote.renoteId != null) {
-				gnote.renote = await this.noteEntityService.pack(gnote.renoteId, this.user!, {
-					detail: true,
-				});
-			}
+		}
+		// Renoteなら再pack
+		if (note.renoteId != null) {
+			note.renote = await this.noteEntityService.pack(note.renoteId, this.user, {
+				detail: true,
+			});
 		}
 
-		// Ignore notes from instances the user has muted
-		if (isInstanceMuted(gnote, new Set<string>(this.userProfile!.mutedInstances ?? []))) return;
-
 		// 関係ない返信は除外
-		if (gnote.reply && !this.withReplies) {
-			const reply = gnote.reply;
+		if (note.reply && this.user && !this.withReplies) {
+			const reply = note.reply;
 			// 「チャンネル接続主への返信」でもなければ、「チャンネル接続主が行った返信」でもなければ、「投稿者の投稿者自身への返信」でもない場合
-			if (reply.userId !== this.user!.id && note.userId !== this.user!.id && reply.userId !== note.userId) return;
+			if (reply.userId !== this.user.id && note.userId !== this.user.id && reply.userId !== note.userId) return;
 		}
 
 		// 流れてきたNoteがミュートしているユーザーが関わるものだったら無視する
-		if (isUserRelated(gnote, this.userIdsWhoMeMuting)) return;
+		if (isUserRelated(note, this.userIdsWhoMeMuting)) return;
 		// 流れてきたNoteがブロックされているユーザーが関わるものだったら無視する
-		if (isUserRelated(gnote, this.userIdsWhoBlockingMe)) return;
+		if (isUserRelated(note, this.userIdsWhoBlockingMe)) return;
 
-		if (gnote.renote && !gnote.text && isUserRelated(gnote, this.userIdsWhoMeMutingRenotes)) return;
+		if (note.renote && !note.text && isUserRelated(note, this.userIdsWhoMeMutingRenotes)) return;
 
 		// 流れてきたNoteがミュートすべきNoteだったら無視する
 		// TODO: 将来的には、単にMutedNoteテーブルにレコードがあるかどうかで判定したい(以下の理由により難しそうではある)
 		// 現状では、ワードミュートにおけるMutedNoteレコードの追加処理はストリーミングに流す処理と並列で行われるため、
 		// レコードが追加されるNoteでも追加されるより先にここのストリーミングの処理に到達することが起こる。
 		// そのためレコードが存在するかのチェックでは不十分なので、改めてcheckWordMuteを呼んでいる
-		if (this.userProfile && await checkWordMute(gnote, this.user, this.userProfile.mutedWords)) return;
+		if (this.userProfile && await checkWordMute(note, this.user, this.userProfile.mutedWords)) return;
 
-		this.connection.cacheNote(gnote);
+		this.connection.cacheNote(note);
 
-		this.send('note', gnote);
+		this.send('note', note);
 	}
 
 	@bindThis
-	public dispose(): void {
+	public dispose() {
 		// Unsubscribe events
 		this.subscriber.off('notesStream', this.onNote);
 	}
