ce391f86d0f0b4862bf9aaa58cf04619ec0e63c7
Suzulu <zukkiyb.iu2@gmail.com>
2023-08-12 17:52:24 +0900

    「棲み分け」に変更

--- packages/frontend/src/pages/channel.vue
+++ packages/frontend/src/pages/channel.vue
@@ -12,7 +12,7 @@
 						<div><i class="ti ti-users ti-fw"></i><I18n :src="i18n.ts._channel.usersCount" tag="span" style="margin-left: 4px;"><template #n><b>{{ channel.usersCount }}</b></template></I18n></div>
 						<div><i class="ti ti-pencil ti-fw"></i><I18n :src="i18n.ts._channel.notesCount" tag="span" style="margin-left: 4px;"><template #n><b>{{ channel.notesCount }}</b></template></I18n></div>
 					</div>
-					<div v-if="channel.isSensitive" :class="$style.sensitiveIndicator">{{ i18n.ts.sensitive }}</div>
+					<div v-if="channel.isSensitive" :class="$style.sensitiveIndicator">{{ i18n.ts.compartmentalization }}</div>
 					<div :class="$style.bannerFade"></div>
 				</div>
 				<div v-if="channel.description" :class="$style.description">
