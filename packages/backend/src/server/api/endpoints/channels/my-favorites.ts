ad0239893f006734da832265322263b1d6fdc5b3
Suzulu <zukkiyb.iu2@gmail.com>
2023-09-09 05:48:50 +0900

    コピペミスを修正

--- packages/backend/src/server/api/endpoints/channels/my-favorites.ts
+++ packages/backend/src/server/api/endpoints/channels/my-favorites.ts
@@ -42,7 +42,17 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 	) {
 		super(meta, paramDef, async (ps, me) => {
 			const query = this.channelFavoritesRepository.createQueryBuilder('favorite')
-				.andWhere('favorite.userId = :meId', { meId: me.id })
+			.andWhere('favorite.userId = :meId', { meId: me.id })
+				.andWhere(new Brackets(qb => { qb
+					.where('channel.isPrivate = FALSE')
+					.orWhere(new Brackets(qb2 => { qb2
+						.where('channel.isPrivate = TRUE')
+						.andWhere(new Brackets(qb3 => { qb3
+							.where(':id = ANY(channel.privateUserIds)', { id: me?.id })
+							.orWhere('channel.userId = :id', { id: me?.id });
+						}));
+					}));
+				}))
 				.leftJoinAndSelect('favorite.channel', 'channel');
 
 			const favorites = await query
