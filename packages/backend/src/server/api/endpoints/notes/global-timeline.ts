57b80b38b660ed9e163f6bf1e13faaeac187ad3a
Suzulu <zukkiyb.iu2@gmail.com>
2023-09-09 06:57:37 +0900

    Ver3.1.0

--- packages/backend/src/server/api/endpoints/notes/global-timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/global-timeline.ts
@@ -139,7 +139,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 				.andWhere(new Brackets(qb =>{
 					qb.where(`(note.renoteCount > :FolloweeRenoteCount) `,{ FolloweeRenoteCount: FolloweeRenoteCount })
 					.orWhere(`(note.userHost IS NULL) AND (note.renoteCount > :LocalRenoteCount)`, { LocalRenoteCount: LocalRenoteCount })
-					.orWhere(`(note.score > :FolloweeRenoteCount) AND note.userId IN (:...meOrFolloweeIds) `, { meOrFolloweeIds: meOrFolloweeIds, FolloweeRenoteCount: FolloweeRenoteCount });
+					.orWhere(`(note.renoteCount > :FolloweeRenoteCount) AND note.userId IN (:...meOrFolloweeIds) `, { meOrFolloweeIds: meOrFolloweeIds, FolloweeRenoteCount: FolloweeRenoteCount });
 				 }));
 			} else {
 				query.andWhere(`(note.userHost IS NULL) AND (note.score > 30)`);
