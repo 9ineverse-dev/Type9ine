07803cdf7e6638fdcd04eb5821d61c3706f33341
Suzulu <zukkiyb.iu2@gmail.com>
2023-09-09 05:33:19 +0900

    コピペミスを修正

--- packages/backend/src/server/api/endpoints/channels/my-favorites.ts
+++ packages/backend/src/server/api/endpoints/channels/my-favorites.ts
@@ -43,16 +43,17 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 	) {
 		super(meta, paramDef, async (ps, me) => {
 			const query = this.channelFavoritesRepository.createQueryBuilder('favorite')
-			.andWhere(new Brackets(qb => { qb
-				.where('channel.isPrivate = FALSE')
-				.orWhere(new Brackets(qb2 => { qb2
-					.where('channel.isPrivate = TRUE')
-					.andWhere(new Brackets(qb3 => { qb3
-						.where(':id = ANY(channel.privateUserIds)', { id: me?.id })
-						.orWhere('channel.userId = :id', { id: me?.id });
+			.andWhere('favorite.userId = :meId', { meId: me.id })
+				.andWhere(new Brackets(qb => { qb
+					.where('channel.isPrivate = FALSE')
+					.orWhere(new Brackets(qb2 => { qb2
+						.where('channel.isPrivate = TRUE')
+						.andWhere(new Brackets(qb3 => { qb3
+							.where(':id = ANY(channel.privateUserIds)', { id: me?.id })
+							.orWhere('channel.userId = :id', { id: me?.id });
+						}));
 					}));
-				}));
-			}))
+				}))
 				.leftJoinAndSelect('favorite.channel', 'channel');
 
 			const favorites = await query
