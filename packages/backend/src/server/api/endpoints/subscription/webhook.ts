2553b364515a61f81af5892c34b84c23876fd7a2
Suzulu <zukkiyb.iu2@gmail.com>
2023-09-24 09:12:27 +0900

    もうわからん

--- packages/backend/src/server/api/endpoints/subscription/webhook.ts
+++ packages/backend/src/server/api/endpoints/subscription/webhook.ts
@@ -66,10 +66,11 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 				// Retrieve the event by verifying the signature using the raw body and secret.
 				let event;
 				const signature = headers!['stripe-signature'];
+				const req = JSON.stringify(bodys);
 				const buf = bodys!.rawBody;
 				try {
 					event = stripe.webhooks.constructEvent(
-						buf,
+						(req as any).rawBody,
 						signature,
 						webhookSecret);
 				} catch (err) {
@@ -77,8 +78,7 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 						redirect: {
 							construct: 'failed',
 							err: err,
-							//req: ps,
-							reqbuf: bodys,
+							req: (req as any).rawBody,
 						}
 					};
 				}
