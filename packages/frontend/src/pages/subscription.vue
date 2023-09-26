aa9a0dcc7d29ba5d6eabb77e647f23db7655ef50
Suzulu <zukkiyb.iu2@gmail.com>
2023-09-26 15:05:01 +0900

    わからん

--- packages/frontend/src/pages/subscription.vue
+++ packages/frontend/src/pages/subscription.vue
@@ -24,7 +24,7 @@ onMounted(() => {
 	let roleIds = [];
 	for (let role in $i.roles ) {
 	}
-	if ($i && $i.roles.some(r => $i.roles.id.includes(r.id))) {
+	if ($i && $i.roles.some(r => $i.roles.includes(r.id))) {
 		isSubscriptionMember = true;
 	}
 });
