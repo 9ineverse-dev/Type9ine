79052249717751c94749518fb2965196071f011e
Suzulu <zukkiyb.iu2@gmail.com>
2023-10-01 08:45:50 +0900

    merge2023.9.3その7

--- packages/frontend/src/pages/admin/roles.vue
+++ packages/frontend/src/pages/admin/roles.vue
@@ -103,18 +103,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 							</MkSwitch>
 						</MkFolder>
 
-						<MkFolder v-if="matchQuery([i18n.ts._role._options.canCreatePrivateChannel, 'canCreatePrivateChannel'])">
-							<template #label>{{ i18n.ts._role._options.canCreatePrivateChannel }}</template>
-							<template #suffix>{{ policies.canCreatePrivateChannel ? i18n.ts.yes : i18n.ts.no }}</template>
-							<MkSwitch v-model="policies.canCreatePrivateChannel">
-								<template #label>{{ i18n.ts.enable }}</template>
-							</MkSwitch>
-						</MkFolder>
-
-						<MkFolder v-if="matchQuery([i18n.ts._role._options.canAccountDelete, 'canAccountDelete'])">
-							<template #label>{{ i18n.ts._role._options.canAccountDelete }}</template>
-							<template #suffix>{{ policies.canAccountDelete ? i18n.ts.yes : i18n.ts.no }}</template>
-							<MkSwitch v-model="policies.canAccountDelete">
 						<MkFolder v-if="matchQuery([i18n.ts._role._options.canUseTranslator, 'canSearchNotes'])">
 							<template #label>{{ i18n.ts._role._options.canUseTranslator }}</template>
 							<template #suffix>{{ policies.canUseTranslator ? i18n.ts.yes : i18n.ts.no }}</template>
