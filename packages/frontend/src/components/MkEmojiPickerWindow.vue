cd74fcb3b1b02bf4ab92535a551ee67c8f536523
Suzulu <zukkiyb.iu2@gmail.com>
2024-03-15 02:44:14 +0900

    絵文字開けない問題を解決

---
+++ packages/frontend/src/components/MkEmojiPickerWindow.vue
@@ -0,0 +1,57 @@
+<!--
+SPDX-FileCopyrightText: syuilo and misskey-project
+SPDX-License-Identifier: AGPL-3.0-only
+-->
+
+<template>
+	<MkWindow
+		ref="window"
+		:initialWidth="300"
+		:initialHeight="290"
+		:canResize="true"
+		:mini="true"
+		:front="true"
+		@closed="emit('closed')"
+	>
+		<MkEmojiPicker
+			:class="$style.picker"
+			:showPinned="showPinned"
+			:pinnedEmojis="pinnedEmojis"
+			:asReactionPicker="asReactionPicker"
+			:targetNote="targetNote"
+			asWindow
+			@chosen="chosen"
+		/>
+	</MkWindow>
+	</template>
+
+	<script lang="ts" setup>
+	import * as Misskey from 'misskey-js';
+	import MkWindow from '@/components/MkWindow.vue';
+	import MkEmojiPicker from '@/components/MkEmojiPicker.vue';
+
+	withDefaults(defineProps<{
+		src?: HTMLElement;
+		showPinned?: boolean;
+		pinnedEmojis?: string[],
+		asReactionPicker?: boolean;
+		targetNote?: Misskey.entities.Note
+	}>(), {
+		showPinned: true,
+	});
+
+	const emit = defineEmits<{
+		(ev: 'chosen', v: any): void;
+		(ev: 'closed'): void;
+	}>();
+
+	function chosen(emoji: any) {
+		emit('chosen', emoji);
+	}
+	</script>
+
+	<style lang="scss" module>
+	.picker {
+		height: 100%;
+	}
+	</style>
